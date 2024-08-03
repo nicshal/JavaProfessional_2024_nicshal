d2f21d8b0eae8f67c709f608dd91d78f09263ef1
Nikolay Shalashov <nicshal@mail.ru>
2024-08-03 17:12:40 +0300

    hw04-garbage-collector: Определение нужного размера хипа

--- hw04-garbage-collector/build.gradle.kts
+++ hw04-garbage-collector/build.gradle.kts
@@ -1,23 +1,3 @@
 dependencies {
     implementation ("ch.qos.logback:logback-classic")
 }
-
-/*
-plugins {
-    id("java")
-}
-
-group = "ru.nicshal"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}*/
